2b56f6886e5cfee07ed675d776eab860531d1b25
atoder <alexandert@gmail.com>
2017-09-06 21:54:24 -0700

    JS Basics - JavaScript Operator Precedence & Associativity

---
+++ JavaScript Basics/008_javascript_operator_precedence_and_associativity/devdelight.js
@@ -0,0 +1,17 @@
+// Operator Precedence 
+var total = (1 + 2) * 1;
+console.log(total);
+
+// Operator Associativity
+
+
+// Assignment
+var a = 1, b = 2, c = 3;
+
+a = b = c;
+
+console.log('a,b,c:', a, b, c);
+
+var total2 = 3 + 2 + 1;
+console.log(total2);
+
