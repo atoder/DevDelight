5cb5f0df5d33cdda2595b69f6970f4ae00ab88b0
atoder <alexandert@gmail.com>
2017-11-25 16:05:49 -0800

    New Videos

---
+++ JavaScript Basics/016_javascript_string_manipulation_and_methods_part_2_of_2/devdelight.js
@@ -0,0 +1,49 @@
+var brand = 'DevDelight.com - Development Done Right';
+
+// 1) charAt
+console.log(brand.charAt(0));
+
+// 2) split
+console.log(brand.split(' '));
+
+// 3) slice
+console.log(brand.slice(17));
+console.log(brand.slice(17, 20));
+console.log(brand.slice(-1));
+
+// 4) substring
+console.log(brand.substring(17, 20));
+
+// 5) substr
+console.log(brand.substr(17, 3));
+
+// 6) trim
+console.log(' Hey, this is DevDelight.com '.trim());
+
+// 7) concat
+console.log('devdelight'.concat('.com'));
+
+// 8) toLowerCase()
+console.log('THIS IS DEVDELIGHT.com'.toLowerCase());
+
+// 9) toUpperCase()
+console.log('this is devdelight.com'.toUpperCase());
+
+// 10) endsWith();
+console.log('Hello world'.endsWith('world'));
+
+// 11) includes()
+console.log('Hello world'.includes('Hello'));
+
+// 12) indexOf
+console.log('Hello world'.indexOf('world'));
+
+// 13) lastIndexOf
+console.log('Hello world'.lastIndexOf('o'));
+
+// 14) repeat()
+console.log('Hello'.repeat(3));
+
+// 15) search()
+console.log('Hello'.search('l'));
+
