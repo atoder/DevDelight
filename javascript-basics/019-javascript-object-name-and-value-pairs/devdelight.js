a86fa4df0847570fedd7d57628f74651b6376cfc
atoder <alexandert@gmail.com>
2019-07-19 20:59:46 -0700

    adding source code for 19 and 20

---
+++ javascript-basics/019-javascript-object-name-and-value-pairs/devdelight.js
@@ -0,0 +1,23 @@
+
+
+var name = 'Alexander';
+console.log(name);
+
+// bag with related information
+// or collection of name and values
+var personObj = {
+  name: 'Alexander',
+  gender: 'male',
+  style: 'savage',
+  location: {
+    city: 'San Francisco',
+    state: 'California',
+    zipcode: 94118
+  }
+};
+// var personObj = new Object();
+// var personObj = {};
+// personObj.name = 'Alexander';
+console.log(personObj['name']);
+console.log(personObj['location']['zipcode']);
+console.log(personObj.style);
---
+++ javascript-basics/020-javascript-how-to-remove-property-from-objects/devdelight.js
@@ -0,0 +1,42 @@
+var personObj = {
+  name: 'Alexander',
+  gender: null,
+  style: 'savage',
+  location: {
+    city: 'San Francisco',
+    state: 'California',
+    zipcode: 94118
+  }
+};
+//console.log(personObj['name']);
+
+// How do I remove a property from JavaScript
+//console.log(personObj);
+
+// 1st way
+delete personObj.name;
+
+// 2nd
+delete personObj['location']['zipcode'];
+console.log(personObj);
+
+// 3rd
+let deleteProperty = 'city';
+delete personObj[deleteProperty];
+
+// How do I check if a property is in an object?
+
+// 1st
+if (personObj.gender) {
+  console.log('true');
+}
+
+// 2nd
+if (personObj.hasOwnProperty('gender')) {
+  console.log('true');
+}
+
+// 3rd
+if ('gender' in personObj) {
+  console.log('true');
+}
