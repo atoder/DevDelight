31d9e769b6ba6abfce3e84cf65038c48b29f2380
atoder <alexandert@gmail.com>
2019-08-11 11:05:44 -0700

    Adding style guide and use strict

---
+++ javascript-basics/022-javascript-style-guide-and-naming-conventions/devdelight.js
@@ -0,0 +1,10 @@
+let firstName = 'Alex';
+function sayHello() {
+  console.log('hello');
+}
+
+let personObj = {
+  name: 'Alex',
+  city: 'San Francisco'
+}
+
---
+++ javascript-basics/023-javascript-use-strict/devdelight.js
@@ -0,0 +1,26 @@
+
+/*
+ * let randomVariable = 'DevDelight';
+ * console.log(randomVariable);
+ *
+ * function printUrl() {
+ *   let urlOfTheWebsite = 'http://www.devdelight.com';
+ *   console.log(urlOfTheWebsite);
+ * }
+ *
+ * printUrl();
+ */
+
+/*
+ *function printStr(str, str) {
+ *  'use strict';
+ *  console.log(str);
+ *}
+ *printStr('test', 'test');
+ */
+
+
+
+
+
+
