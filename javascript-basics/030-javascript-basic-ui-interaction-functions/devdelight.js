fbb5d788b1677cebc142abee351a545145c667ef
atoder <alexandert@gmail.com>
2019-08-21 18:56:16 -0700

    basic ui interfaction functions

---
+++ javascript-basics/030-javascript-basic-ui-interaction-functions/devdelight.js
@@ -0,0 +1,8 @@
+//alert('Welcome to devdelight!');
+//let userInput = confirm('Are you a developer?');
+//console.log(userInput);
+
+let firstName = prompt('What is your name?', 'Alex');
+console.log(firstName);
+
+
